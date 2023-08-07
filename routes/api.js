ac257ef72b5aeca7a2cf6a196488832c44b8cba6
ll-nick <github@le-large.net>
2023-08-07 23:18:55 +0200

    implement post api

--- routes/api.js
+++ routes/api.js
@@ -13,8 +13,34 @@ module.exports = function (app) {
       res.json(issues);
     })
     
-    .post(function (req, res){
-      let project = req.params.project;
+    .post(async (req, res) => {
+      try {
+        let newIssue = await saveNewIssue(
+          req.params.project,
+          req.body.issue_title,
+          req.body.issue_text,
+          req.body.created_by,
+          req.body.assigned_to,
+          req.body.status_text
+        );
+
+        let resJson = {
+          issue_title: newIssue.issue_title,
+          issue_text: newIssue.issue_text,
+          created_by: newIssue.created_by,
+          assigned_to: newIssue.assigned_to,
+          status_text: newIssue.status_text,
+          created_on: newIssue.created_on,
+          updated_on: newIssue.updated_on,
+          open: newIssue.open,
+          _id: newIssue._id
+        }
+
+        res.json(resJson)
+
+      } catch (err) {
+        res.json({ error: err.message });
+      }
       
     })
     
