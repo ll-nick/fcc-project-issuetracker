5912f9cbe48483a8785fbb651772b4711823dfe4
ll-nick <github@le-large.net>
2023-08-07 23:33:22 +0200

    post functional tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -5,6 +5,72 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-suite('Functional Tests', function() {
-  
+suite('Functional Tests', function () {
+  test('Post valid issue with optional fields', function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .post('/api/issues/test_project')
+      .send({
+        issue_title: "a",
+        issue_text: "b",
+        created_by: "c",
+        assigned_to: "d",
+        status_text: "e"
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.issue_title, 'a')
+        assert.equal(res.body.issue_text, 'b')
+        assert.equal(res.body.created_by, 'c')
+        assert.equal(res.body.assigned_to, 'd')
+        assert.equal(res.body.status_text, 'e')
+        assert.equal(res.body.open, true)
+        assert.isNotNull(res.body.created_on)
+        assert.isNotNull(res.body.updated_on)
+        done();
+      });
+  });
+
+  test('Post valid issue without optional fields', function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .post('/api/issues/test_project')
+      .send({
+        issue_title: "a",
+        issue_text: "b",
+        created_by: "c",
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.issue_title, 'a')
+        assert.equal(res.body.issue_text, 'b')
+        assert.equal(res.body.created_by, 'c')
+        assert.equal(res.body.assigned_to, '')
+        assert.equal(res.body.status_text, '')
+        assert.equal(res.body.open, true)
+        assert.isNotNull(res.body.created_on)
+        assert.isNotNull(res.body.updated_on)
+        done();
+      });
+  });
+
+  test('Post valid issue without required fields', function (done) {
+    chai
+      .request(server)
+      .keepOpen()
+      .post('/api/issues/test_project')
+      .send({
+        issue_title: "a",
+      })
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.equal(res.type, 'application/json');
+        assert.equal(res.body.error, 'required field(s) missing')
+        done();
+      });
+  });
 });
